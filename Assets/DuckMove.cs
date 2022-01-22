053bb24f256d6a5b319251df5f6e84d0b8c2dc1a
PQPower <warcrai14881@rambler.ru>
2022-01-22 22:25:53 +0300

    Сделал: 1) Переименовал объект Cube(что был уткой) в Duck, добавил в неё Rigidbody2D, Animator. 2) Настроил аниматор у утки, добавив 5 анимаций (Afk, DownMove, LeftMove, UpMove, RightMove) 2 из которых идентичны (afk и downmove) по очевидным причинам. Добавил 4 bool переменных,что переключают анимации, через них настроил переходы между анимациями. 3) Добавил скрипт, в котором сделал 2 метода: Move() и ChangeMovingSprite(). Как понятно из названий в мув прописано как утка двигается (причём ещё и на кроссплатформенном уровне), а в ChangeMovingSprite() Прописаны условия для которых и меняются эти самые 4 bool переменных, переключая анимации

---
+++ Assets/DuckMove.cs
@@ -0,0 +1,103 @@
+using System.Collections;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+public class DuckMove : MonoBehaviour
+{
+    [SerializeField] float runSpeed = 15f; //Специальные поля, которыми можно управлять внутри юнити
+    private Rigidbody2D myRigidBody2D;
+    private Animator myAnimator;
+    void Start()
+    {
+        myRigidBody2D = GetComponent<Rigidbody2D>();
+        myAnimator = GetComponent<Animator>();
+    }
+    private void Move()
+    {
+        float controlThrowY= CrossPlatformInputManager.GetAxis("Horizontal");
+        float controlThrowX = CrossPlatformInputManager.GetAxis("Vertical");
+        Vector2 duckVelocity = new Vector2(controlThrowY* runSpeed, controlThrowX * runSpeed);
+        myRigidBody2D.velocity = duckVelocity;
+     
+   
+    }
+    private void ChangeMovingSprite()
+    {
+        float horizontalRight= CrossPlatformInputManager.GetAxis("Horizontal");
+        if (horizontalRight > Mathf.Epsilon)
+        {
+            myAnimator.SetBool("rightMoveBool", true);
+        }
+        else
+            myAnimator.SetBool("rightMoveBool", false);
+
+
+        float horizontalLeft = CrossPlatformInputManager.GetAxis("Horizontal");
+        if (horizontalLeft < -Mathf.Epsilon)
+        {
+            myAnimator.SetBool("leftMoveBool", true);
+        }
+        else
+            myAnimator.SetBool("leftMoveBool", false);
+
+
+        float verticalUp = CrossPlatformInputManager.GetAxis("Vertical");
+        if (verticalUp > Mathf.Epsilon)
+        {
+            myAnimator.SetBool("upMoveBool", true);
+        }
+        else
+            myAnimator.SetBool("upMoveBool", false);
+
+
+        float verticalDown = CrossPlatformInputManager.GetAxis("Vertical"); ;
+        if (verticalDown < -Mathf.Epsilon)
+        {
+            myAnimator.SetBool("downMoveBool", true);
+        }
+        else
+            myAnimator.SetBool("downMoveBool", false);
+
+        //bool horizontalRight = (myRigidBody2D.velocity.x) > Mathf.Epsilon;
+        //if (horizontalRight)
+        //{
+        //    myAnimator.SetBool("rightMoveBool", true);
+        //}
+        //else
+        //myAnimator.SetBool("rightMoveBool", false);
+
+
+        //bool horizontalLeft = (myRigidBody2D.velocity.x) < - Mathf.Epsilon;
+        //if (horizontalLeft)
+        //{
+        //    myAnimator.SetBool("leftMoveBool", true);
+        //}
+        //else
+        //    myAnimator.SetBool("leftMoveBool", false);
+
+
+        //bool verticalUp = (myRigidBody2D.velocity.y) > Mathf.Epsilon;
+        //if (verticalUp)
+        //{
+        //    myAnimator.SetBool("upMoveBool", true);
+        //}
+        //else
+        //    myAnimator.SetBool("upMoveBool", false);
+
+
+        //bool verticalDown = (myRigidBody2D.velocity.y) < - Mathf.Epsilon;
+        //if (verticalDown)
+        //{
+        //    myAnimator.SetBool("downMoveBool", true);
+        //}
+        //else
+        //    myAnimator.SetBool("downMoveBool", false);
+
+    }
+    void Update()
+    {
+        Move();
+        ChangeMovingSprite();
+    }
+}
